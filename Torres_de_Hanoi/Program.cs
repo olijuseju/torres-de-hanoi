2a1e99bb3601972fffd4b63f4430088d515b1dde
Jose Julio <jjpeajar@epsg.upv.es>
2021-03-09 11:28:49 +0100

    Menú de program actualizado a prueba de fallos

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,39 +10,54 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
-
+            bool valido = false;
             Console.WriteLine("Buenos días, inserte el número de discos por favor :)");
-            int totalDiscos = Int32.Parse(Console.ReadLine());
-            int formula = (int)(Math.Pow(2, totalDiscos) - 1);
-            List<Disco> Elementos = new List<Disco>();
-            for (int i=totalDiscos; i>0; i--)
-            {
-                Elementos.Add(new Disco(i));
+            while (!valido)
+            { //Este While comprueba que hayas introducido un número válido
+                try
+                {
+                    int totalDiscos = Int32.Parse(Console.ReadLine());
+
+                    int formula = (int)(Math.Pow(2, totalDiscos) - 1);
+                    List<Disco> Elementos = new List<Disco>();
+                    for (int i = totalDiscos; i > 0; i--)
+                    {
+                        Elementos.Add(new Disco(i));
+                    }
+
+                    Pila pilaIni = new Pila(Elementos);
+                    Pila pilaAux = new Pila();
+                    Pila pilaFin = new Pila();
+
+
+                    Hanoi El_problema_de_hanoi = new Hanoi();
+
+                    int numMovimientos = El_problema_de_hanoi.iterativo(totalDiscos, pilaIni, pilaFin, pilaAux);
+
+                    Console.WriteLine("El problema con " + totalDiscos + " discos se ha resuelto en " + numMovimientos + " movimientos.");
+
+                    if (numMovimientos == formula)
+                    {
+                        Console.WriteLine("El problema se ha resuelto bien :)");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El problema se ha resuelto mal :(");
+
+                    }
+                    valido = true; //Si todo va bien, salimos del while
+                    Console.WriteLine("Pulse cualquier tecla para salir");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Por favor, ingrese un número válido");
+                    //Si ha ingresado un número negativo, o una tecla o cualquier otro símbolo volvemos a empezar
+                }
             }
-
-            Pila pilaIni = new Pila(Elementos);
-            Pila pilaAux = new Pila();
-            Pila pilaFin = new Pila();
-
-
-            Hanoi El_problema_de_hanoi = new Hanoi();
-
-            int numMovimientos = El_problema_de_hanoi.iterativo(totalDiscos, pilaIni, pilaFin, pilaAux);
-
-            Console.WriteLine("El problema con " + totalDiscos + " discos se ha resuelto en " + numMovimientos + " movimientos.");
-
-            if (numMovimientos ==  formula)
-            {
-                Console.WriteLine("El problema se ha resuelto bien :)");
-            }
-            else
-            {
-                Console.WriteLine("El problema se ha resuelto mal :(");
-
-            }
-
-            Console.WriteLine("Pulse cualquier tecla para salir");
-            Console.ReadKey();
+            
+           
 
         }
     }
